be5bd94111754249f89d05574ecfd9a1f8f79364
Mansi-Atey <mansiatey31@gmail.com>
2021-03-23 12:41:50 +0530

    [mansi]Added: code for adding 56,30 and 70

---
+++ Adding/Adding/LinkedList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adding
+{
+    class LinkedList
+    {
+        internal Node head;
+        public LinkedList()
+        {
+            this.head = null;
+        }
+        internal void Add(int item)
+        {
+            Node node = new Node(item);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                node.next = head;
+                head = node;
+            }
+        }
+        internal void Display()
+        {
+            if (head == null)
+                System.Console.WriteLine("List is Empty");
+            else
+            {
+                Node temp = head;
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
--- Adding/Adding/Node.cs
+++ Adding/Adding/Node.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace LinkedListUC
+namespace Adding
 {
     class Node
     {
